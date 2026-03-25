a271af88eef4bacece2540ab3d838bac4795f466
Shkembo69420 <m.radev1337@gmail.com>
2026-03-25 21:23:57 +0200

    made class public

--- restaurant-management-system.2/Domain/Entities/Order.cs
+++ restaurant-management-system.2/Domain/Entities/Order.cs
@@ -7,7 +7,7 @@ using System.Threading.Tasks;
 
 namespace restaurant_management_system._2.Domain.Entities
 {
-    internal class Order
+    public class Order
     {
         public Table Table { get; set; }
         public List<OrderItem> Items { get; set; }
