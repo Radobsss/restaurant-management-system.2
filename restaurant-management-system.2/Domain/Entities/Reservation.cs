c4aa266b3b03c5f2c012664d75bb5b08ec6cf584
Shkembo69420 <m.radev1337@gmail.com>
2026-03-25 20:55:23 +0200

    Add more entities for menu and staff

---
+++ restaurant-management-system.2/Domain/Entities/Reservation.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace restaurant_management_system._2.Domain.Entities
+{
+    internal class Reservation
+    {
+    }
+}
