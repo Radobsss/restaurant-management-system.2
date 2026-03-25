6e75502f7c4a9911d2acbccd926ea6107d04d236
Shkembo69420 <m.radev1337@gmail.com>
2026-03-25 21:25:08 +0200

    connection to staffrole

--- restaurant-management-system.2/Domain/Entities/Staff.cs
+++ restaurant-management-system.2/Domain/Entities/Staff.cs
@@ -3,10 +3,12 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using restaurant_management_system._2.Domain.Enums;
 
 namespace restaurant_management_system._2.Domain.Entities
 {
-    internal class Staff
+    public class Staff
     {
+        public StaffRole Role { get; set; }
     }
 }
