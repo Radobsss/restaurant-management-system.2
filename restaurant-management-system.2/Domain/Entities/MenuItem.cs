8fb9adb57f5948b00abdf56e2d066adfddf516e8
Shkembo69420 <m.radev1337@gmail.com>
2026-03-25 21:16:36 +0200

    menu connections

--- restaurant-management-system.2/Domain/Entities/MenuItem.cs
+++ restaurant-management-system.2/Domain/Entities/MenuItem.cs
@@ -3,10 +3,14 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using restaurant_management_system._2.Domain.Enums;
+
 
 namespace restaurant_management_system._2.Domain.Entities
 {
-    internal class MenuItem
+    public class MenuItem
     {
+        public Category Category { get; set; }
+        public MenuItemType Type { get; set; }
     }
 }
