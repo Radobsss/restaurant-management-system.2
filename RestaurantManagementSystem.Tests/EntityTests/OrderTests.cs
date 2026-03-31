248fa3b21b194ce84f6b9a570561a1877e21d579
bosss rado <radobonev386@gmail.com>
2026-03-31 18:15:25 +0300

    adding first test for order

--- RestaurantManagementSystem.Tests/EntityTests/OrderTests.cs
+++ RestaurantManagementSystem.Tests/EntityTests/OrderTests.cs
@@ -1,12 +1,25 @@
-﻿using System;
+﻿using restaurant_management_system._2.Domain.Entities;
+using restaurant_management_system._2.Domain.Enums;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-
+using Microsoft.VisualStudio.TestTools.UnitTesting;
 namespace RestaurantManagementSystem.Tests.EntityTests
 {
-    internal class OrderTests
+    [TestClass]
+    public class OrderTests
     {
+        [TestMethod]
+            public void Order_ShouldInitializeItemsList()
+            {
+                var order = new Order();
+
+                Assert.IsNotNull(order.Items);
+            }
+           
     }
 }
+
+
