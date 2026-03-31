36c07b2adb63db38f5a4fd7033738bec8ce69c02
Shkembo69420 <m.radev1337@gmail.com>
2026-03-31 17:25:03 +0300

    Table_ShouldSetAndGetPropertiesCorrectly test

--- RestaurantManagementSystem.Tests/EntityTests/TableTests.cs
+++ RestaurantManagementSystem.Tests/EntityTests/TableTests.cs
@@ -25,5 +25,22 @@ namespace RestaurantManagementSystem.Tests.EntityTests
 
             Assert.IsNotNull(table.Reservations);
         }
+
+        [TestMethod]
+        public void Table_ShouldSetAndGetPropertiesCorrectly()
+        {
+            var table = new Table
+            {
+                Id = 1,
+                Number = 5,
+                Capacity = 4,
+                IsOccupied = true
+            };
+
+            Assert.AreEqual(1, table.Id);
+            Assert.AreEqual(5, table.Number);
+            Assert.AreEqual(4, table.Capacity);
+            Assert.IsTrue(table.IsOccupied);
+        }
     }
 }
