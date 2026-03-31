df1765549917a4eb512725dfd3efffb19b6fab49
Shkembo69420 <m.radev1337@gmail.com>
2026-03-31 17:12:50 +0300

    Table_ShouldInitializeOrdersList test

--- RestaurantManagementSystem.Tests/EntityTests/TableTests.cs
+++ RestaurantManagementSystem.Tests/EntityTests/TableTests.cs
@@ -3,10 +3,19 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using restaurant_management_system._2.Domain.Entities;
 
 namespace RestaurantManagementSystem.Tests.EntityTests
 {
-    internal class TableTests
+    [TestClass]
+    public class TableTests
     {
+        [TestMethod]
+        public void Table_ShouldInitializeOrdersList()
+        {
+            var table = new Table();
+
+            Assert.IsNotNull(table.Orders);
+        }
     }
 }
