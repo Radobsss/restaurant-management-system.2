d47bd3fa6f2e3ba6dacc1fde5bd175c6cc34943e
bosss rado <radobonev386@gmail.com>
2026-04-21 18:44:44 +0300

    adding create order method

--- restaurant-management-system.2/Service/OrderService.cs
+++ restaurant-management-system.2/Service/OrderService.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using restaurant_management_system._2.Domain.Entities;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +7,34 @@ using System.Threading.Tasks;
 
 namespace restaurant_management_system._2.Service
 {
-    internal class OrderService
+    public class OrderService
     {
+        private readonly List<Order> orders = new List<Order>();
+        private readonly TableService tableService;
+
+        public OrderService(TableService tableService)
+        {
+            this.tableService = tableService;
+        }
+
+        public Order CreateOrder(int tableId)
+        {
+            Table table = tableService.GetTableById(tableId);
+
+            if (table.IsOccupied)
+                throw new InvalidOperationException("Table is already occupied.");
+
+            Order order = new Order
+            {
+                Id = orders.Count + 1,
+                TableId = tableId,
+                CreatedAt = DateTime.Now
+            };
+
+            orders.Add(order);
+            table.IsOccupied = true;
+
+            return order;
+        }
     }
 }
