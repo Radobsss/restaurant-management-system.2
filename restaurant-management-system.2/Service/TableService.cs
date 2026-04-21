f84fe7d1f6124fbfaed024083b5503f1f3f66754
bosss rado <radobonev386@gmail.com>
2026-04-21 18:16:23 +0300

    simple add table method

--- restaurant-management-system.2/Service/TableService.cs
+++ restaurant-management-system.2/Service/TableService.cs
@@ -1,12 +1,31 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using restaurant_management_system._2.Domain.Entities;
 
 namespace restaurant_management_system._2.Service
 {
-    internal class TableService
+    public class TableService
     {
+        private readonly List<Table> tables = new List<Table>();
+
+        public Table AddTable(int capacity, string location)
+        {
+            if (capacity <= 0)
+                throw new ArgumentException("Capacity must be greater than 0.");
+
+            if (location == null || location == "")
+            {
+                throw new ArgumentException("Location cannot be empty.");
+            }
+
+            Table table = new Table
+            {
+                Id = tables.Count + 1,
+                Capacity = capacity,
+                Location = location,
+                IsOccupied = false
+            };
+
+            tables.Add(table);
+            return table;
+        }
     }
 }
