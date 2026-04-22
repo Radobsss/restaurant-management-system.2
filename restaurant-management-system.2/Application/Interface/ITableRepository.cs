481361fc23ee2c6de99fdd5bb30733722c1d51e0
radobosss <radostinbonev10@gmail.com>
2026-04-22 10:24:56 +0300

    adding table repo

---
+++ restaurant-management-system.2/Application/Interface/ITableRepository.cs
@@ -0,0 +1,13 @@
+﻿using RestaurantProject.Domain.Entities;
+using System.Collections.Generic;
+
+namespace RestaurantProject.Application.Interfaces
+{
+    public interface ITableRepository
+    {
+        List<Table> GetAll();
+        Table GetById(int id);
+        Table GetByNumber(int number);
+        void Save(Table table);
+    }
+}
