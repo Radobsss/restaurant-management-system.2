3f2eb67e6a6aeb2eae96e64642d9b91710e30f4a
bosss rado <radobonev386@gmail.com>
2026-04-25 14:23:59 +0300

    making them public

--- restaurant-management-system.2/Infrastructure/FileTableRepository.cs
+++ restaurant-management-system.2/Infrastructure/FileTableRepository.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 
 namespace restaurant_management_system._2.Infrastructure
 {
-    internal class FileTableRepository
+    public class FileTableRepository
     {
     }
 }
