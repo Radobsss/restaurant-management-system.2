e20e2017c9d1a95f432e5c5ea33336af7f1f2796
radobosss <radostinbonev10@gmail.com>
2026-04-29 09:18:20 +0300

    Add table repository using FileStorage

--- restaurant-management-system.2/Infrastructure/FileTableRepository.cs
+++ restaurant-management-system.2/Infrastructure/FileTableRepository.cs
@@ -1,12 +1,63 @@
-﻿using System;
+﻿using restaurant_management_system._2.Domain.Application.Interfaces;
+using restaurant_management_system._2.Domain.Entities;
+using restaurant_management_system._2.Infrastructure;
 using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
-namespace restaurant_management_system._2.Infrastructure
+namespace restaurant_management_system._2.Infrastructure.Repositories
 {
-    internal class FileTableRepository
+    public class FileTableRepository : ITableRepository
     {
+        private readonly FileStorage storage;
+
+        public FileTableRepository(FileStorage storage)
+        {
+            this.storage = storage;
+        }
+
+        public List<Table> GetAll()
+        {
+            TableStorage db = storage.Load();
+            return db.Tables;
+        }
+
+        public Table GetById(int id)
+        {
+            TableStorage db = storage.Load();
+            return db.Tables.FirstOrDefault(t => t.Id == id);
+        }
+
+        public Table GetByNumber(int number)
+        {
+            TableStorage db = storage.Load();
+            return db.Tables.FirstOrDefault(t => t.Number == number);
+        }
+
+        public void Save(Table table)
+        {
+            TableStorage db = storage.Load();
+
+            if (table.Id == 0)
+            {
+                table.Id = db.NextId;
+                db.NextId++;
+
+                db.Tables.Add(table);
+            }
+            else
+            {
+                Table existingTable = db.Tables.FirstOrDefault(t => t.Id == table.Id);
+
+                if (existingTable != null)
+                {
+                    existingTable.Number = table.Number;
+                    existingTable.Capacity = table.Capacity;
+                    existingTable.Location = table.Location;
+                    existingTable.IsOccupied = table.IsOccupied;
+                }
+            }
+
+            storage.Save(db);
+        }
     }
 }
