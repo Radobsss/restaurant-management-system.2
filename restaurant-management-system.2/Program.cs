2369ac50773a42449289fd79f0d1b3f0de4b7675
radobosss <radostinbonev10@gmail.com>
2026-04-29 09:29:22 +0300

    testing if the thing that we have work

--- restaurant-management-system.2/Program.cs
+++ restaurant-management-system.2/Program.cs
@@ -1,1 +1,38 @@
-﻿
+﻿using System;
+using restaurant_management_system._2.Domain.Entities;
+using restaurant_management_system._2.Infrastructure;
+
+FileStorage storage = new FileStorage();
+
+TableStorage db = storage.Load();
+
+db.Tables.Add(new Table
+{
+    Id = db.NextId++,
+    Number = 1,
+    Capacity = 4,
+    Location = "Main hall",
+    IsOccupied = false
+});
+
+db.Tables.Add(new Table
+{
+    Id = db.NextId++,
+    Number = 2,
+    Capacity = 2,
+    Location = "Window",
+    IsOccupied = false
+});
+
+db.Tables.Add(new Table
+{
+    Id = db.NextId++,
+    Number = 3,
+    Capacity = 6,
+    Location = "Terrace",
+    IsOccupied = true
+});
+
+storage.Save(db);
+
+Console.WriteLine("Tables saved successfully!");
