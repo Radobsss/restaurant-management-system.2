5537ce06b2f72bc75a35092aa4a369c3ec65429a
radobosss <radostinbonev10@gmail.com>
2026-04-29 09:17:21 +0300

    Implement FileStorage with Load and Save methods

---
+++ restaurant-management-system.2/Infrastructure/FileStorage.cs
@@ -0,0 +1,25 @@
+﻿using System.IO;
+using System.Text.Json;
+
+namespace restaurant_management_system._2.Infrastructure
+{
+    public class FileStorage
+    {
+        private readonly string path = "data.json";
+
+        public TableStorage Load()
+        {
+            if (!File.Exists(path))
+                return new TableStorage();
+
+            string json = File.ReadAllText(path);
+            return JsonSerializer.Deserialize<TableStorage>(json);
+        }
+
+        public void Save(TableStorage data)
+        {
+            string json = JsonSerializer.Serialize(data);
+            File.WriteAllText(path, json);
+        }
+    }
+}
