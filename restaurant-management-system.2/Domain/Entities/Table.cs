62d042a6c639b1209bf93447d8d59784bcd9f8e9
radobosss <radostinbonev10@gmail.com>
2026-04-29 09:15:54 +0300

    Add Name field to Table model

--- restaurant-management-system.2/Domain/Entities/Table.cs
+++ restaurant-management-system.2/Domain/Entities/Table.cs
@@ -10,11 +10,11 @@ namespace restaurant_management_system._2.Domain.Entities
     {
         public List<Order> Orders { get; set; }  = new List<Order>();
         public List<Reservation> Reservations { get; set; } = new List<Reservation>();
-
         public int Id { get; set; }
         public int Number { get; set; }
         public int Capacity { get; set; }
         public bool IsOccupied { get; set; }
         public string Location { get; set; }
+       
     }
 }
