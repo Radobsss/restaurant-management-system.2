97e3249ce4bcd6d705a72e4eedb7c9d4e7818fd1
radobosss <radostinbonev10@gmail.com>
2026-04-29 09:19:00 +0300

    Add storage model for TableStorage

--- restaurant-management-system.2/Infrastructure/TableStorage.cs
+++ restaurant-management-system.2/Infrastructure/TableStorage.cs
@@ -1,12 +1,15 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.Collections.Generic;
+using restaurant_management_system._2.Domain.Entities;
 
 namespace restaurant_management_system._2.Infrastructure
 {
-    internal class TableStorage
+    public class TableStorage
     {
+        public int NextId { get; set; } = 1;
+
+        public List<Table> Tables { get; set; } = new List<Table>();
+        public List<Order> Orders { get; set; } = new List<Order>();
+        public List<Category> Categories { get; set; } = new List<Category>();
+        public List<MenuItem> MenuItems { get; set; } = new List<MenuItem>();
     }
 }
