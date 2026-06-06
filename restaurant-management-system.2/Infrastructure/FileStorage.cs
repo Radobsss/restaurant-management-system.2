017fe803398f5826a1f489d8da3697394508fbd0
bosss rado <radobonev386@gmail.com>
2026-06-06 12:36:10 +0300

    remove unused json storage files

--- restaurant-management-system.2/Infrastructure/FileStorage.cs
+++ restaurant-management-system.2/Infrastructure/FileStorage.cs
@@ -1,26 +1,26 @@
-﻿using restaurant_management_system._2.Infrastructure;
-using System.IO;
-using System.Text.Json;
+﻿//using restaurant_management_system._2.Infrastructure;
+//using System.IO;
+//using System.Text.Json;
 
-namespace restaurant_management_system._2.Infrastructure
-{
-    public class FileStorage
-    {
-        private readonly string path = "data.json";
+//namespace restaurant_management_system._2.Infrastructure
+//{
+//    public class FileStorage
+//    {
+//        private readonly string path = "data.json";
 
-        public TableStorage Load()
-        {
-            if (!File.Exists(path))
-                return new TableStorage();
+//        public TableStorage Load()
+//        {
+//            if (!File.Exists(path))
+//                return new TableStorage();
 
-            string json = File.ReadAllText(path);
-            return JsonSerializer.Deserialize<TableStorage>(json);
-        }
+//            string json = File.ReadAllText(path);
+//            return JsonSerializer.Deserialize<TableStorage>(json);
+//        }
 
-        public void Save(TableStorage data)
-        {
-            string json = JsonSerializer.Serialize(data);
-            File.WriteAllText(path, json);
-        }
-    }
-}
+//        public void Save(TableStorage data)
+//        {
+//            string json = JsonSerializer.Serialize(data);
+//            File.WriteAllText(path, json);
+//        }
+//    }
+//}
--- restaurant-management-system.2/Infrastructure/TableStorage.cs
+++ restaurant-management-system.2/Infrastructure/TableStorage.cs
@@ -1,15 +1,15 @@
-﻿using System.Collections.Generic;
-using restaurant_management_system._2.Domain.Entities;
+﻿//using System.Collections.Generic;
+//using restaurant_management_system._2.Domain.Entities;
 
-namespace restaurant_management_system._2.Infrastructure
-{
-    public class TableStorage
-    {
-       // public int NextId { get; set; } = 1;
+//namespace restaurant_management_system._2.Infrastructure
+//{
+//    public class TableStorage
+//    {
+//       // public int NextId { get; set; } = 1;
 
-        public List<Table> Tables { get; set; } = new List<Table>();
-        public List<Order> Orders { get; set; } = new List<Order>();
-        public List<Category> Categories { get; set; } = new List<Category>();
-        public List<MenuItem> MenuItems { get; set; } = new List<MenuItem>();
-    }
-}
+//        public List<Table> Tables { get; set; } = new List<Table>();
+//        public List<Order> Orders { get; set; } = new List<Order>();
+//        public List<Category> Categories { get; set; } = new List<Category>();
+//        public List<MenuItem> MenuItems { get; set; } = new List<MenuItem>();
+//    }
+//}
