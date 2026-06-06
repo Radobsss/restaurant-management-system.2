0bb04c685e2decc9b3074bf1dbc8f6e07c9661b4
bosss rado <radobonev386@gmail.com>
2026-06-06 11:58:08 +0300

    add reservation repository

---
+++ restaurant-management-system.2/Application/Interface/IReservationRepository.cs
@@ -0,0 +1,17 @@
+﻿using restaurant_management_system._2.Domain.Entities;
+
+namespace restaurant_management_system._2.Application.Interface
+{
+    public interface IReservationRepository
+    {
+        List<Reservation> GetAll();
+
+        Reservation? GetById(int id);
+
+        List<Reservation> GetByTableId(int tableId);
+
+        void Add(Reservation reservation);
+
+        void Update(Reservation reservation);
+    }
+}
---
+++ restaurant-management-system.2/Infrastructure/FileReservationRepository.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using Microsoft.EntityFrameworkCore;
+using restaurant_management_system._2.Application.Interface;
+using restaurant_management_system._2.Domain.Entities;
+using restaurant_management_system._2.Infrastructure.Data;
+
+namespace restaurant_management_system._2.Infrastructure
+{
+    public class FileReservationRepository : IReservationRepository
+    {
+        private readonly RestaurantDbContext db;
+
+        public FileReservationRepository(RestaurantDbContext db)
+        {
+            this.db = db;
+        }
+
+        public List<Reservation> GetAll()
+        {
+            return db.Reservations
+                .Include(r => r.Table)
+                .OrderBy(r => r.StartTime)
+                .ToList();
+        }
+
+        public Reservation? GetById(int id)
+        {
+            return db.Reservations
+                .Include(r => r.Table)
+                .FirstOrDefault(r => r.Id == id);
+        }
+
+        public List<Reservation> GetByTableId(int tableId)
+        {
+            return db.Reservations
+                .Where(r => r.TableId == tableId)
+                .OrderBy(r => r.StartTime)
+                .ToList();
+        }
+
+        public void Add(Reservation reservation)
+        {
+            db.Reservations.Add(reservation);
+            db.SaveChanges();
+        }
+
+        public void Update(Reservation reservation)
+        {
+            db.Reservations.Update(reservation);
+            db.SaveChanges();
+        }
+    }
+}
